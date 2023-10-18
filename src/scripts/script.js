87782f3d047b1f29dce80c0438fe587e8004eea2
pavanturlapati <pavanturlapati@gmail.com>
2023-10-18 08:04:53 -0400

    Add files via upload

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,9 @@
+// JavaScript for handling tab switching
+function openTab(tabName) {
+    const tabs = document.getElementsByClassName("tab");
+    for (const tab of tabs) {
+        tab.style.display = "none";
+    }
 
+    document.getElementById(tabName).style.display = "block";
+}
---
+++ src/scripts/tasks.js
@@ -0,0 +1,83 @@
+var tasksData = [
+  {
+    task: "Create a personal portfolio page and save in git",
+    status: "In Progress",
+    completion: 60,
+  },
+  {
+    task: "Create and save personal Java - Cucumber framework in Git",
+    status: "In Progress",
+    completion: 10,
+  },
+  {
+    task: "Create and save personal Java - Selenium framework in Git",
+    status: "In Progress",
+    completion: 10,
+  },
+  {
+    task: "Create and save personal Java - Appium framework in Git",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Create and save personal Java - Rest Assured framework in Git",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Create and save personal Java - Design Patterns framework in Git",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Create and save personal Cypress tool framework in Git",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Create and save personal Robot framework in Git",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Create and save personal Selenium - Python framework in Git",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Refresh knowledge on Tosca tool",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Refresh knowledge on Test Manager ISTQB Certification",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Practice for the AZ-900 certification",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Practice for the Cloud Digital Leader certification",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Practice for the AWS Cloud Practitioner",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Learn and practice HTML 5 - Udemy course",
+    status: "In Progress",
+    completion: 0,
+  },
+  {
+    task: "Learn and practice Javascript - Udemy course",
+    status: "In Progress",
+    completion: 0,
+  },
+  // Add more tasks here
+];
